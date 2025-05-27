1ee77d7c4a1ad828496f1c2009eff144382dd4e1
Santino Vasquez <santinorhv@hotmail.com>
2025-05-27 20:13:20 -0300

    Update ejercicio_1.py
    ejercicio 1 numpy resuelto

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     # Realizar una funcion lambda que eleve al cuadrado
     # el número pasado como parámetro
 
-    # potencia_2 = lambda x:......
+    potencia_2 = lambda x: 2**x
     # pot_3 = potencia_2(3)
 
     # 2)
@@ -26,33 +26,17 @@ if __name__ == '__main__':
     # que retorne la potencia de 2 de cada numero en la lista numeros
     # El resultado (la potencia de cada numero) se debe ir almacenando
     # en una nueva lista
+    numeros = [1, -5, 4, 3]
+    numeros_abs = list(map(abs, numeros))
+    print(numeros_abs)
+    numeros_potenciados = list(map(potencia_2, numeros))
+    
+    numeros.append(numeros_potenciados)
+    print(numeros_potenciados)
     # Nota: realizar la lambda expression "in line", es decir,
     # no declarar la lambda fuera del map sino diretamente dentro
     # Copiar la lambda creada en el paso anterior dentro del map
     # NOTA: No debe usar "potencia_2" dentro del map, debe colocar
     # directamente la lambda.
 
-    # Lista de numeros
-    numeros = [1, -5, 4, 3]
-
-    # numeros_potencia = list(map....)
-
-    # Desarrollo de 1) ***************************
-    
-    potencia_2 = lambda x: x**2
-    pot_3 = potencia_2(3)
-
-    print('El número 3 elevado al cuadrado es:',pot_3)
-
-    # Desarrollo de 2) ***************************
-
-    numeros_map = map(potencia_2, numeros)
-    numeros_map = list(numeros_map)
-
-    print(numeros_map)
-    
-    numeros_lambda = list(map(lambda x: x**2, numeros))
-
-    print(numeros_lambda)
-
-    print("terminamos")
+ 
