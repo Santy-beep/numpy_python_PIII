cc88c40e44159a18d34dc7e0fbfb7d5b2ace235a
Santino Vasquez <santinorhv@hotmail.com>
2025-05-28 18:04:53 -0300

    Update ejercicio_2.py
    ejercicio 2 realizado

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -39,16 +39,9 @@ if __name__ == '__main__':
     # Desarrollo de 1) ***************************
 
     len_string = lambda x: len(x)
-   
-    print(len_string('jesus gonzalez')) 
 
     # Desarrollo de 2) ***************************
 
-    palabras_len = map(len_string, palabras)
-    palabras_len = list(palabras_len)
-
-    print(palabras_len)
-    
     palabras_len = list(map(lambda x: len(x), palabras))
 
     print(palabras_len)
